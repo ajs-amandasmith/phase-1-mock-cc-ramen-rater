3346534c735a4a1ffe2f0b6a89753c9652883c8c
Amanda Smith <ajs.amandasmith@gmail.com>
2022-04-05 13:48:21 -0500

    fetched/get ramen data from db.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 // write your code here
 
+document.addEventListener('DOMContentLoaded', () => {
+  getRamenData();
+})
+
+function getRamenData() {
+  fetch('http://localhost:3000/ramens')
+    .then(response => response.json())
+    .then(data => showRamenData(data))
+}
+
+function showRamenData(ramenArray) {
+  console.log(ramenArray);
+}
 
 
-
-
-
-// As a user, I can:
+// see ramen images in div#ramen-menu
 
 // See all ramen images in the div with the id of ramen-menu. When the page loads, request the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
 // Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the #ramen-detail div and where it says insert comment here and insert rating here.
