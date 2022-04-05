68344e7e5cbeb901b56ff883e4f4891414d7dafd
Amanda Smith <ajs.amandasmith@gmail.com>
2022-04-05 13:57:13 -0500

    display ramen images in #ramen-menu div

--- src/index.js
+++ src/index.js
@@ -11,13 +11,25 @@ function getRamenData() {
 }
 
 function showRamenData(ramenArray) {
-  console.log(ramenArray);
+  console.log('ramen data', ramenArray);
+  const ramenMenu = document.getElementById('ramen-menu');
+  console.log('menu', ramenMenu)
+  ramenArray.map(ramen => {
+    console.log('ramen', ramen)
+    const ramenImage = document.createElement('img');
+
+    ramenImage.src = ramen.image;
+
+    ramenMenu.append(ramenImage);
+  })
 }
 
 
 // see ramen images in div#ramen-menu
+  // page loads -> request data from server to get all the ramen objects
+  // display the image for each of the ramen using an img tag inside #ramen-menu div
 
-// See all ramen images in the div with the id of ramen-menu. When the page loads, request the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
+
 // Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the #ramen-detail div and where it says insert comment here and insert rating here.
 // Create a new ramen after submitting the new-ramen form. The new ramen should be added to the#ramen-menu div. The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
 
